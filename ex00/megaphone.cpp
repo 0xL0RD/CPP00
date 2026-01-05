4aa4d1cabcd3e3022151b736b0dc5fa898d17e99
Rbs <rubsanch@student.42madrid.com>
2026-01-05 09:14:35 +0100

    check if string is empty

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 17:50:22 by rubsanch          #+#    #+#             */
-/*   Updated: 2026/01/05 08:51:40 by rubsanch         ###   ########.fr       */
+/*   Updated: 2026/01/05 09:13:53 by rubsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,44 +22,35 @@ int	replace_space(std::string str)
 	if (str.length() < 2)
 		return (0);
 	pos = 0;
-	//std::cout << str << std::endl;
 	while (1)
 	{
 		pos = str.find_first_of(space, pos);
-		//std::cout << pos << std::endl;
-		//std::cout << "size: " << str.length() << std::endl;
-		//std::cout << "pos : " << pos << std::endl;
 		if (pos == std::string::npos || pos == str.length() - 1)
 		{
-			//std::cout << "break" << std::endl;
 			break;
 		}
 		pos++;
-		//std::cout << "next" << std::endl;
 	}
-	//std::cout << "-----------------" << std::endl;
 	while (1)
 	{
 		pos = str.find("  ");
 		if (pos == std::string::npos)
 		{
-			//std::cout << "break2" << std::endl;
 			break;
 		}
 		str.replace(pos, 2, " ");
 	}
 	if (str.find_first_of(space) == 0)
 		str.erase(0, 1);
-	//std::cout << "len3: " << str.length() << std::endl;
+	if (str.length() < 1)
+		return (0);
 	if (str.find_last_of(space, std::string::npos) == str.length() - 1)
 		str.erase(str.length() - 1);
-	//std::cout << "len4: " << str.length() << std::endl;
 	if (str.length() < 1)
 		return (0);
 	for (long unsigned int i = 0; i <= str.length() - 1; i++)
 		str.at(i) = toupper(str.at(i));
 	std::cout << str << std::endl;
-	//std::cout << "2-----------------" << std::endl;
 	return (1);
 }
 
@@ -83,6 +74,5 @@ int	main(int argc, char *argv[])
 	{
 		default_msg();	
 	}
-	//std::cout << params << std::endl;
 	return (0);
 }
