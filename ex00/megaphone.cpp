3dd9ef73685fd8d8395d69c26cc2768c7600ae6e
Rbs <rubsanch@student.42madrid.com>
2026-01-05 07:45:38 +0100

    empty params, trim spaces

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: rubsanch <rubsanch@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 17:50:22 by rubsanch          #+#    #+#             */
-/*   Updated: 2026/01/04 19:13:23 by rubsanch         ###   ########.fr       */
+/*   Updated: 2026/01/05 07:44:42 by rubsanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 #include <string>
 #include <cstddef>
 
-void	replace_space(std::string str)
+int	replace_space(std::string str)
 {
 	size_t	pos;
 	const std::string space = " \f\n\r\t\v";
 
+	if (str.length() < 2)
+		return (0);
 	pos = 0;
 	std::cout << str << std::endl;
 	while (1)
@@ -26,9 +28,14 @@ void	replace_space(std::string str)
 		pos = str.find_first_of(space, pos);
 		std::cout << pos << std::endl;
 		std::cout << "size: " << str.length() << std::endl;
-		if (pos == str.length() - 1)
+		std::cout << "pos : " << pos << std::endl;
+		if (pos == std::string::npos || pos == str.length() - 1)
+		{
+			std::cout << "break" << std::endl;
 			break;
+		}
 		pos++;
+		std::cout << "next" << std::endl;
 		//str.replace(pos, -1, " ");
 	}
 	std::cout << "-----------------" << std::endl;
@@ -39,24 +46,46 @@ void	replace_space(std::string str)
 		//if (pos == 18446744073709551615)
 		//if (pos == (size_t) -1)
 		if (pos == std::string::npos)
+		{
+			std::cout << "break2" << std::endl;
 			break;
+		}
 		str.replace(pos, 2, " ");
-		pos++;
+		//pos++;
 	}
 	//str.replace(str.begin(), str.end(), "I
+	//if (*str.rbegin() == " ")
+	//if (str.at(str.length() - -1) == isspace)
+	//if (str[9] == " ")
+	if (str.find_first_of(space) == 0)
+		str.erase(0, 1);
+	std::cout << "len3: " << str.length() << std::endl;
+	if (str.find_last_of(space, std::string::npos) == str.length() - 1)
+		str.erase(str.length() - 1);
+	std::cout << "len4: " << str.length() << std::endl;
+	if (str.length() < 1)
+		return (0);
+	for (long unsigned int i = 0; i <= str.length() - 1; i++)
+		str.at(i) = toupper(str.at(i));
 	std::cout << str << std::endl;
 	std::cout << "2-----------------" << std::endl;
+	return (1);
+}
+
+void	default_msg(void)
+{
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 }
 
 int	main(int argc, char *argv[])
 {
 	int	i;
-	int	j;
+	//int	j;
 	std::string params = "";
 
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		default_msg();
 		return (0);
 	}
 	i = 1;
@@ -65,7 +94,10 @@ int	main(int argc, char *argv[])
 		params.append(std::string(argv[i])).append(" ");;
 		i++;
 	}
-	replace_space(params);
+	if (replace_space(params) == 0)
+	{
+		default_msg();	
+	}
 	std::cout << params << std::endl;
 	return (0);
 }
